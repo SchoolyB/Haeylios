df4710087b6f6549aa043e0a2301b8d3e752a61e
Schooly <marshallbcodes@gmail.com>
2023-05-29 16:22:06 -0500

    Initial Commit

---
+++ src/main/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class Main {
+  public static void main(String[] args) {
+    JFrame window = new JFrame("Hello World");
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    window.setResizable(false); // prevents resizing
+    window.setTitle("RPG GAME");
+
+    GamePanel gamePanel = new GamePanel();
+    window.add(gamePanel); // add gamePanel to window
+    window.pack(); // set window size to preferred size of gamePanel
+    window.setLocationRelativeTo(null); // centers window
+    window.setVisible(true);
+  }
+}
+
